d0ed7264c017698674526f8ef9f0bd1950ea2074
Ane Diaz de Tuesta <anediaz@gmail.com>
2020-09-27 10:34:58 +0200

    adjust photo sizes for API call depending on device type

--- src/services/photosUtils.js
+++ src/services/photosUtils.js
@@ -1,14 +1,48 @@
 import { SIZE_LABELS, EXTRAS } from "../constants/constants";
 
-const getFamiliesSizes = () => ({
-  def: EXTRAS.large1024,
-  big: EXTRAS.large1024,
-});
+const getDeviceType = () => {
+  const ua = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "tablet";
+  }
+  if (
+    /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+      ua
+    )
+  ) {
+    return "mobile";
+  }
+  return "desktop";
+};
 
-const getMiniFamiliesSizes = () => ({
-  def: SIZE_LABELS.SMALL,
-  big: SIZE_LABELS.LARGE,
-});
+const getFamiliesSizes = () => {
+  const device = getDeviceType();
+
+  if (device === "mobile") {
+    return {
+      def: EXTRAS.small320,
+      big: EXTRAS.original,
+    };
+  }
+  return {
+    def: EXTRAS.large1024,
+    big: EXTRAS.large1024,
+  };
+};
+
+const getMiniFamiliesSizes = () => {
+  const device = getDeviceType();
+  if (device === "mobile") {
+    return {
+      def: SIZE_LABELS.SMALL320,
+      big: SIZE_LABELS.MEDIUM,
+    };
+  }
+  return {
+    def: SIZE_LABELS.SMALL,
+    big: SIZE_LABELS.LARGE,
+  };
+};
 
 const getFamiliesConfigurations = () => [
   { minWidth: 480, cols: 3, margin: 1 },
