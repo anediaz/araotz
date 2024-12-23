a6b21d1e596ee3030fe75cedd8984a701198b289
Ane Diaz de Tuesta <ane.diazdetuesta@datadoghq.com>
2024-12-23 09:19:26 +0100

    New version, without styled components

---
+++ precache-manifest.52da1e5f1a0d4bf98726be980f21f48a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98211cac9108c139178897dc63a99f94",
+    "url": "/araotz/index.html"
+  },
+  {
+    "revision": "1639d3ab0f988faa7636",
+    "url": "/araotz/static/css/main.d6a64194.chunk.css"
+  },
+  {
+    "revision": "7b08a6460c89dfe8fdc6",
+    "url": "/araotz/static/js/2.96eb9bc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/araotz/static/js/2.96eb9bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1639d3ab0f988faa7636",
+    "url": "/araotz/static/js/main.1e3d495b.chunk.js"
+  },
+  {
+    "revision": "880f8019b8d5daa02ae1",
+    "url": "/araotz/static/js/runtime-main.0f7e6e14.js"
+  },
+  {
+    "revision": "b22ffc77f706ebb9f998841c7ce43f0d",
+    "url": "/araotz/static/media/araotz.b22ffc77.png"
+  }
+]);
